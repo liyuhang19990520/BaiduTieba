7e7877b4c1291be772c738e72817e0c40fbedac4
liyuhang19990520 <1092680144@qq.com>
2020-10-22 09:03:27 +0800

    项目

--- register-login/register-code/api/user/username.js
+++
@@ -1,9 +0,0 @@
-const USER = require('../../model/USER')
-var a;
-function username(req,res) {
-    if(req.body.username){
-        a = req.body.username
-    }
-    res.send(a);
-}
-module.exports = username
